0643285f297eb4f058dcfb1a87c20ff827c90971
Daniel Mc <danielmc122333@icloud.com>
2022-04-15 15:42:51 +0100

    first commit

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,56 +1,121 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-import MovieForm from './components/MovieForm';
-import UserForm from './components/UserForm';
+/** @format */
 
-const apiUrl = 'http://localhost:4000';
+import { useEffect, useState } from "react"
+import "./App.css"
+import MovieForm from "./components/MovieForm"
+import UserForm from "./components/UserForm"
+
+const apiUrl = "http://localhost:4000"
 
 function App() {
-  const [movies, setMovies] = useState([]);
+  const [movies, setMovies] = useState([])
+  const [registeredResponse, setRegisteredResponse] = useState()
+  const [loginResponse, setLoginResponse] = useState()
 
   useEffect(() => {
     fetch(`${apiUrl}/movie`)
-      .then(res => res.json())
-      .then(res => setMovies(res.data));
-  }, []);
+      .then((res) => res.json())
+      .then((res) => setMovies(res.data))
+  }, [])
 
   const handleRegister = async ({ username, password }) => {
-    
-  };
+    const options = {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        username: username,
+        password: password,
+      }),
+    }
+    fetch("http://localhost:4000/user/register", options)
+      .then((res) => res.json())
+      .then((json) =>
+        setRegisteredResponse(
+          "You have successfully registered with:" + json.data.username
+        )
+      )
+      .catch(() => {
+        setRegisteredResponse("Server error")
+      })
+  }
 
   const handleLogin = async ({ username, password }) => {
-    
-  };
-  
+    const options = {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        username: username,
+        password: password,
+      }),
+    }
+
+    fetch("http://localhost:4000/user/login", options)
+      .then((res) => res.json())
+      .then((json) => {
+        setLoginResponse("Logged In with token:" + json.data)
+        localStorage.setItem("jwt", json.data)
+      })
+  }
+
   const handleCreateMovie = async ({ title, description, runtimeMins }) => {
-    
+    const options = {
+      method: "POST",
+      headers: {
+        Authorization: "Bearer" + localStorage.getItem("jwt"),
+      },
+      body: JSON.stringify({
+        title: title,
+        description: description,
+        runtimeMins: runtimeMins,
+      }),
+    }
+
+    fetch("http://localhost:4000/movie", options)
+      .then((res) => res.json())
+      .then((json) => {
+        if (res.ok) {
+          setMovies(json.data)
+        } else {
+          console.log("Invalid response code:", res.status)
+          console.log("Invalid response data:", json)
+        }
+      })
+      .catch((e) => {
+        console.log("Unable to contact server:", e)
+      })
   }
 
   return (
     <div className="App">
       <h1>Register</h1>
       <UserForm handleSubmit={handleRegister} />
+      <p>{registeredResponse}</p>
 
       <h1>Login</h1>
       <UserForm handleSubmit={handleLogin} />
+      <p>{loginResponse}</p>
 
       <h1>Create a movie</h1>
       <MovieForm handleSubmit={handleCreateMovie} />
 
       <h1>Movie list</h1>
       <ul>
-        {movies.map(movie => {
+        {movies.map((movie) => {
           return (
             <li key={movie.id}>
               <h3>{movie.title}</h3>
               <p>Description: {movie.description}</p>
               <p>Runtime: {movie.runtimeMins}</p>
             </li>
-          );
+          )
         })}
       </ul>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/server/controllers/user.js
+++ src/server/controllers/user.js
@@ -1,39 +1,52 @@
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
+/** @format */
 
-const jwtSecret = 'mysecret';
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
+const { PrismaClient } = require("@prisma/client")
+const prisma = new PrismaClient()
+
+const jwtSecret = "mysecret"
 
 const register = async (req, res) => {
-    const { username, password } = req.body;
+  const { username, password } = req.body
 
-    const createdUser = null;
+  const hashedPassword = await bcrypt.hash(password, 10)
 
-    res.json({ data: createdUser });
-};
+  const createdUser = await prisma.user.create({
+    data: {
+      username: username,
+      password: hashedPassword,
+    },
+  })
+
+  res.json({ data: createdUser })
+}
 
 const login = async (req, res) => {
-    const { username, password } = req.body;
+  const { username, password } = req.body
 
-    const foundUser = null;
+  const foundUser = await prisma.user.findFirst({
+    where: {
+      username,
+    },
+  })
 
-    if (!foundUser) {
-        return res.status(401).json({ error: 'Invalid username or password.' });
-    }
+  if (!foundUser) {
+    return res.status(401).json({ error: "Invalid username or password." })
+  }
 
-    const passwordsMatch = false;
+  const passwordsMatch = await bcrypt.compare(password, foundUser.password)
 
-    if (!passwordsMatch) {
-        return res.status(401).json({ error: 'Invalid username or password.' });
-    }
+  if (!passwordsMatch) {
+    return res.status(401).json({ error: "Invalid username or password." })
+  }
 
-    const token = null;
+  const token = jwt.sign({ username }, jwtSecret)
 
-    res.json({ data: token });
-};
+  res.json({ data: token })
+}
 
 module.exports = {
-    register,
-    login
-};
+  register,
+  login,
+}
