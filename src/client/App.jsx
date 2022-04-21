73e26f58e5f3e4c9d8ba7a6532f16d6f67df08dd
Safia <safia.ibrahim2121@gmail.com>
2022-04-21 09:38:50 +0100

    complete

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,30 +1,87 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-import MovieForm from './components/MovieForm';
-import UserForm from './components/UserForm';
+import { useEffect, useState } from "react";
+import "./App.css";
+import MovieForm from "./components/MovieForm";
+import UserForm from "./components/UserForm";
 
-const apiUrl = 'http://localhost:4000';
+const apiUrl = "http://localhost:4000";
 
 function App() {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
     fetch(`${apiUrl}/movie`)
-      .then(res => res.json())
-      .then(res => setMovies(res.data));
+      .then((res) => res.json())
+      .then((res) => setMovies(res.data));
   }, []);
 
   const handleRegister = async ({ username, password }) => {
-    
+    const options = {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify({
+        username: username,
+        password: password,
+      }),
+    };
+    fetch("http://localhost:4000/user/register", options)
+      .then((res) => res.json())
+      .then((json) => {
+        console.log(json.data);
+      });
   };
 
   const handleLogin = async ({ username, password }) => {
-    
+    const options = {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        username: username,
+        password: password,
+      }),
+    };
+    fetch("http://localhost:4000/user/login", options)
+      .then((res) => res.json())
+      .then((json) => {
+        console.log(json.data);
+        localStorage.setItem("fred", json.data);
+      });
   };
-  
+
   const handleCreateMovie = async ({ title, description, runtimeMins }) => {
-    
-  }
+    const options = {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        Authorization: "Bearer " + localStorage.getItem("fred"),
+      },
+      body: JSON.stringify({
+        title: title,
+        description: description,
+        runtimeMins: runtimeMins,
+      }),
+    };
+    fetch("http://localhost:4000/movie", options)
+      .then((res) => {
+        res.json().then((json) => {
+          if (res.ok) {
+            console.log("created movie: ", json);
+            const updateMovieList = movies.map(function (movie) {
+              if (movie === data.json) {
+                return setMovies(updateMovieList);
+              }
+            });
+          } else {
+            console.log("Invalid response code: ", res.status);
+            console.log("Invalid response data: ", json);
+          }
+        });
+      })
+      .catch((error) =>
+        console.log("Unable to connect to the server: ", error)
+      );
+  };
 
   return (
     <div className="App">
@@ -39,7 +96,7 @@ function App() {
 
       <h1>Movie list</h1>
       <ul>
-        {movies.map(movie => {
+        {movies.map((movie) => {
           return (
             <li key={movie.id}>
               <h3>{movie.title}</h3>
--- src/server/controllers/movie.js
+++ src/server/controllers/movie.js
@@ -1,31 +1,42 @@
-const jwt = require('jsonwebtoken');
-const { PrismaClient } = require('@prisma/client');
+const jwt = require("jsonwebtoken");
+const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
-const jwtSecret = 'mysecret';
+const jwtSecret = "mysecret";
 
 const getAllMovies = async (req, res) => {
-    const movies = await prisma.movie.findMany();
+  const movies = await prisma.movie.findMany({});
 
-    res.json({ data: movies });
+  res.json({ data: movies });
 };
 
 const createMovie = async (req, res) => {
-    const { title, description, runtimeMins } = req.body;
+  console.log("request: ", req.body);
+  try {
+    const token = req.headers.authorization.substring(7);
+    console.log("token:", token);
+    jwt.verify(token, jwtSecret);
+  } catch (e) {
+    console.log("error:", e);
+    console.log("headers:", req.headers);
+    return res.status(401).json({ error: "Invalid token provided." });
+  }
+  const { title, description, runtimeMins } = req.body;
 
-    try {
-        const token = null;
-        // todo verify the token
-    } catch (e) {
-        return res.status(401).json({ error: 'Invalid token provided.' })
-    }
+  const movieCreated = await prisma.movie.create({
+    data: {
+      title: title,
+      description: description,
+      runtimeMins: runtimeMins,
+    },
+  });
 
-    const createdMovie = null;
+  const createdMovie = movieCreated;
 
-    res.json({ data: createdMovie });
+  res.json({ data: createdMovie });
 };
 
 module.exports = {
-    getAllMovies,
-    createMovie
+  getAllMovies,
+  createMovie,
 };
