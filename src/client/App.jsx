b5fe56dc15f9bbe71adec5aee5ab781a80011762
Joel Watkins <eck087@yahoo.co.uk>
2023-01-25 15:27:34 +0000

    added basic form

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,11 +1,38 @@
-import './App.css';
+import { useState } from "react";
 
-const apiUrl = 'http://localhost:4000';
+import "./App.css";
+
+const apiUrl = "http://localhost:4000";
 
 function App() {
+  const [user, setUser] = useState({ username: "", password: "" });
+  const [movies, setMovies] = useState([]);
+
   return (
     <div className="App">
+      <h2>Register</h2>
+      <form>
+        <input type="text" placeholder="username" />
+        <input type="text" placeholder="password" />
+        <button type="submit">Submit</button>
+      </form>
 
+      <h2>Log in</h2>
+      <form>
+        <input type="text" placeholder="username" />
+        <input type="text" placeholder="password" />
+        <button type="submit">Submit</button>
+      </form>
+
+      <h2>Create a movie</h2>
+      <form>
+        <input type="text" placeholder="title" />
+        <input type="text" placeholder="description" />
+        <input type="text" placeholder="runtime" />
+        <button type="submit">Submit</button>
+      </form>
+
+      <h2>Movie list</h2>
     </div>
   );
 }
