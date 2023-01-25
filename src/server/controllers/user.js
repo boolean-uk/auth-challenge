8472081fe0f2c05c966792706652c3c8eb004ce9
Dave <Daverevell491@gmail.com>
2023-01-25 18:13:21 +0000

    Req. 2 - Server completed

--- src/server/controllers/user.js
+++ src/server/controllers/user.js
@@ -1,6 +1,7 @@
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const { PrismaClient } = require("@prisma/client");
+const { user } = require("../../../../auth-hashing/src/utils/prisma");
 const prisma = new PrismaClient();
 
 const jwtSecret = "mysecret";
@@ -9,7 +10,7 @@ const register = async (req, res) => {
 	const { username, password } = req.body;
 	const saltRounds = 10;
 
-	bcrypt.hash(password, saltRounds, async (err, hashed_pw) => {
+	bcrypt.hash(password, saltRounds, async (hashed_pw) => {
 		try {
 			const newUser = await prisma.user.create({
 				data: {
@@ -33,21 +34,29 @@ const register = async (req, res) => {
 const login = async (req, res) => {
 	const { username, password } = req.body;
 
-	const foundUser = null;
+	const foundUser = await prisma.user.findUnique({
+		where: {
+			username: username,
+		},
+	});
+
+	console.log(foundUser);
 
 	if (!foundUser) {
 		return res.status(401).json({ error: "Invalid username or password." });
 	}
 
-	const passwordsMatch = false;
-
-	if (!passwordsMatch) {
-		return res.status(401).json({ error: "Invalid username or password." });
-	}
-
-	const token = null;
-
-	res.json({ data: token });
+	bcrypt.compare(password, user.password, (passwordsMatch) => {
+		if (!passwordsMatch) {
+			res.status(401).json({ error: "Invalid username or password" });
+		} else {
+			const token = jwt.sign(
+				{ sub: user.id, username: user.username },
+				jwtSecret
+			);
+			res.json({ data: token });
+		}
+	});
 };
 
 module.exports = {
