b8e8f10db2e81aa08767da58cdb05588e720c1b3
wagnerAJS221 <wagneralex221@gmai.com>
2023-01-25 16:40:33 +0000

    not sure if i am on the right track but its something

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,36 +1,60 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-import MovieForm from './components/MovieForm';
-import UserForm from './components/UserForm';
+import { useEffect, useState } from 'react'
+import './App.css'
+import MovieForm from './components/MovieForm'
+import UserForm from './components/UserForm'
 
-const apiUrl = 'http://localhost:4000';
+const apiUrl = 'http://localhost:4000'
 
 function App() {
-  const [movies, setMovies] = useState([]);
+  const [movies, setMovies] = useState([])
+  const [registerNewUser, setregisterNewUser] = useState('')
 
   useEffect(() => {
     fetch(`${apiUrl}/movie`)
-      .then(res => res.json())
-      .then(res => setMovies(res.data));
-  }, []);
+      .then((res) => res.json())
+      .then((res) => setMovies(res.data))
+  }, [])
 
   const handleRegister = async ({ username, password }) => {
-    
-  };
+    // TODO
+    // Send request to register from here alex
+    // You can do it. Just dont stop till you get it right and understand it
 
-  const handleLogin = async ({ username, password }) => {
-    
-  };
-  
-  const handleCreateMovie = async ({ title, description, runtimeMins }) => {
-    
+    const body = {
+      username: username,
+      password: username
+    }
+    console.log('this is before my fetch')
+    useEffect((event) => {
+      event.preventDefault()
+
+      fetch('http://localhost:4000/register', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(body)
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          const key = Object.keys(data)[0]
+          console.log('this is my data', data)
+          if (key === 'username') {
+            setregisterNewUser('Registered succesfully!')
+          } else {
+            setregisterNewUser('Error: Username taken already')
+          }
+        })
+    })
   }
+
+  const handleLogin = async ({ username, password }) => {}
+
+  const handleCreateMovie = async ({ title, description, runtimeMins }) => {}
 
   return (
     <div className="App">
       <h1>Register</h1>
       <UserForm handleSubmit={handleRegister} />
-
+      {registerNewUser && <p>{registerNewUser} </p>}
       <h1>Login</h1>
       <UserForm handleSubmit={handleLogin} />
 
@@ -39,18 +63,18 @@ function App() {
 
       <h1>Movie list</h1>
       <ul>
-        {movies.map(movie => {
+        {movies.map((movie) => {
           return (
             <li key={movie.id}>
               <h3>{movie.title}</h3>
               <p>Description: {movie.description}</p>
               <p>Runtime: {movie.runtimeMins}</p>
             </li>
-          );
+          )
         })}
       </ul>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
