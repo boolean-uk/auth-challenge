7119db3bab48254ada72d2118dc09da08e726a4d
Joel Watkins <eck087@yahoo.co.uk>
2023-01-25 15:46:16 +0000

    started adding controlled inputs

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -6,29 +6,87 @@ const apiUrl = "http://localhost:4000";
 
 function App() {
   const [user, setUser] = useState({ username: "", password: "" });
+  const [movie, setMovie] = useState({
+    title: "",
+    description: "",
+    runtime: "",
+  });
   const [movies, setMovies] = useState([]);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setUser({
+      ...user,
+      [name]: value,
+    });
+
+    console.log(user);
+  };
 
   return (
     <div className="App">
       <h2>Register</h2>
       <form>
-        <input type="text" placeholder="username" />
-        <input type="text" placeholder="password" />
+        <input
+          type="text"
+          placeholder="username"
+          name="username"
+          value={user.username}
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          name="password"
+          value={user.password}
+          onChange={handleChange}
+        />
         <button type="submit">Submit</button>
       </form>
 
       <h2>Log in</h2>
       <form>
-        <input type="text" placeholder="username" />
-        <input type="text" placeholder="password" />
+        <input
+          type="text"
+          placeholder="username"
+          name="username"
+          value={user.username}
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          name="password"
+          value={user.password}
+          onChange={handleChange}
+        />
         <button type="submit">Submit</button>
       </form>
 
       <h2>Create a movie</h2>
       <form>
-        <input type="text" placeholder="title" />
-        <input type="text" placeholder="description" />
-        <input type="text" placeholder="runtime" />
+        <input
+          type="text"
+          placeholder="title"
+          name="title"
+          value={movie.title}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          placeholder="description"
+          name="description"
+          value={movie.description}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          placeholder="runtime"
+          name="runtime"
+          value={movie.runtime}
+          onChange={handleChange}
+        />
         <button type="submit">Submit</button>
       </form>
 
