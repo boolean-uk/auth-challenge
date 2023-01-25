d6040b1362792458b71514e53292ae22486df578
Joel Watkins <eck087@yahoo.co.uk>
2023-01-25 12:47:51 +0000

    registerUser controller complete

--- src/server/controllers/user.js
+++ src/server/controllers/user.js
@@ -1,8 +1,41 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
-const registerUser = (req, res) => {
-  res.send("hello");
+const saltRounds = 10;
+
+const registerUser = async (req, res) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    res.status(400).json({ message: "Please provide a username and password" });
+  }
+
+  bcrypt.genSalt(saltRounds, (err, salt) => {
+    bcrypt.hash(password, salt, async (err, hashedPw) => {
+      try {
+        const newUser = await prisma.user.create({
+          data: {
+            username,
+            password: hashedPw,
+          },
+        });
+
+        delete newUser.password;
+
+        res.status(201).json({ status: "success", user: newUser });
+      } catch (error) {
+        if (error.code === "P2002") {
+          res
+            .status(403)
+            .json({ error: `The username ${username} is already taken` });
+        } else {
+          res.status(500).json({ error });
+        }
+      }
+    });
+  });
 };
 
 module.exports = { registerUser };
