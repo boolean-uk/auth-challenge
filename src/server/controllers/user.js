65f839227bbcb84a654caa829652db8c7f39dcd2
Dave <Daverevell491@gmail.com>
2023-01-25 17:06:31 +0000

    Req. 1 complete

--- src/server/controllers/user.js
+++ src/server/controllers/user.js
@@ -1,39 +1,56 @@
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const { PrismaClient } = require('@prisma/client');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
-const jwtSecret = 'mysecret';
+const jwtSecret = "mysecret";
 
 const register = async (req, res) => {
-    const { username, password } = req.body;
+	const { username, password } = req.body;
+	const saltRounds = 10;
 
-    const createdUser = null;
+	bcrypt.hash(password, saltRounds, async (err, hashed_pw) => {
+		try {
+			const newUser = await prisma.user.create({
+				data: {
+					username,
+					password: hashed_pw,
+				},
+			});
 
-    res.json({ data: createdUser });
+			delete newUser.password;
+			res.status(201).json({ user: newUser });
+		} catch (e) {
+			if (e.code === "P2002") {
+				res.status(403).json({ error: "The username is already taken" });
+			} else {
+				res.status(500).json({ error: e });
+			}
+		}
+	});
 };
 
 const login = async (req, res) => {
-    const { username, password } = req.body;
+	const { username, password } = req.body;
 
-    const foundUser = null;
+	const foundUser = null;
 
-    if (!foundUser) {
-        return res.status(401).json({ error: 'Invalid username or password.' });
-    }
+	if (!foundUser) {
+		return res.status(401).json({ error: "Invalid username or password." });
+	}
 
-    const passwordsMatch = false;
+	const passwordsMatch = false;
 
-    if (!passwordsMatch) {
-        return res.status(401).json({ error: 'Invalid username or password.' });
-    }
+	if (!passwordsMatch) {
+		return res.status(401).json({ error: "Invalid username or password." });
+	}
 
-    const token = null;
+	const token = null;
 
-    res.json({ data: token });
+	res.json({ data: token });
 };
 
 module.exports = {
-    register,
-    login
+	register,
+	login,
 };
