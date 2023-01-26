877b0cca5d6c3eb6f7aa2df2156639b29039082d
Joel Watkins <eck087@yahoo.co.uk>
2023-01-26 10:19:13 +0000

    createMovie server side implemented

---
+++ src/server/controllers/movie.js
@@ -0,0 +1,40 @@
+const jwt = require("jsonwebtoken");
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+const secret = process.env.JWT_SECRET;
+
+const getAllMovies = async (req, res) => {
+  const movies = await prisma.movie.findMany();
+
+  res.json({ movies });
+};
+
+const createMovie = async (req, res) => {
+  const { title, description, runtime } = req.body;
+  let bearer = req.headers.authorization;
+
+  if (!bearer) {
+    return res.status(403).json({ error: "Please log in" });
+  }
+
+  bearer = bearer.replace("Bearer ", "");
+
+  try {
+    const verified = jwt.verify(bearer, secret);
+  } catch (err) {
+    return res.status(401).json({ message: "Invalid token" });
+  }
+
+  const createdMovie = await prisma.movie.create({
+    data: {
+      title,
+      description,
+      runtimeMins: runtime,
+    },
+  });
+
+  res.status(201).json({ status: "success", movie: createdMovie });
+};
+
+module.exports = { getAllMovies, createMovie };
--- src/server/routers/movie.js
+++ src/server/routers/movie.js
@@ -1,8 +1,7 @@
-const express = require('express');
-
-
+const express = require("express");
+const { createMovie, getAllMovies } = require("../controllers/movie");
 const router = express.Router();
 
-
+router.route("/").get(getAllMovies).post(createMovie);
 
 module.exports = router;
