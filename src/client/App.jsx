5a6b9fe9d31689bbf03094d10dc3479fded0843f
unknown <katdemey@outlook.com>
2023-04-03 10:42:48 +0100

    login client complete

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,27 +1,28 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-import MovieForm from './components/MovieForm';
-import UserForm from './components/UserForm';
+import { useEffect, useState } from "react";
+import "./App.css";
+import MovieForm from "./components/MovieForm";
+import UserForm from "./components/UserForm";
 
-const apiUrl = 'http://localhost:4000';
+const apiUrl = "http://localhost:4000";
 
 function App() {
   const [movies, setMovies] = useState([]);
+  //TODO: Check these bits below out further
+  const [registerResponse, setRegisterResponse] = useState("");
+  const [loginResponse, setLoginResponse] = useState("");
 
   useEffect(() => {
     fetch(`${apiUrl}/movie`)
-      .then(res => res.json())
-      .then(res => setMovies(res.data));
+      .then((res) => res.json())
+      .then((res) => setMovies(res.data));
   }, []);
 
   const handleRegister = async ({ username, password }) => {
-    
-
-    if(!username || !password){
-      alert("Please fill in all fields to register")
-      return 
-    } 
-   fetch("http://localhost:4000/user/register", {
+    if (!username || !password) {
+      alert("Please fill in all fields to register");
+      return;
+    }
+    fetch(`${apiUrl}/user/register`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -29,19 +30,35 @@ function App() {
       body: JSON.stringify({ username, password }),
     })
       .then((response) => response.json())
-      .then((data) => setRegisterResponse(data))
+      .then((data) => setRegisterResponse(data));
 
-      UserForm.setUser(data)
-      // console.log("UserForm.user----", UserForm.user)
+    UserForm.setUser(data);
+    // console.log("UserForm.user----", UserForm.user)
   };
 
   const handleLogin = async ({ username, password }) => {
-    
+    if (!username || !password) {
+      alert("Please fill in all fields to register");
+      return;
+    }
+
+    fetch(`${apiUrl}/user/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username, password }),
+    })
+      .then((response) => response.json())
+      .then((data) => setLoginResponse(data.data));
+
+    //    console.log("logged in?", loginResponse);
+
+    const token = JSON.stringify(loginResponse);
+    window.localStorage.setItem("loginResponse", token);
   };
-  
-  const handleCreateMovie = async ({ title, description, runtimeMins }) => {
-    
-  }
+
+  const handleCreateMovie = async ({ title, description, runtimeMins }) => {};
 
   return (
     <div className="App">
@@ -56,7 +73,7 @@ function App() {
 
       <h1>Movie list</h1>
       <ul>
-        {movies.map(movie => {
+        {movies.map((movie) => {
           return (
             <li key={movie.id}>
               <h3>{movie.title}</h3>
