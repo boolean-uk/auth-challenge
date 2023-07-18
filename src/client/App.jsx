7bba83b3e7915c4114c75bf80bfd2078f798dc7d
NoroAxper <norikvardanyan.w@gmail.com>
2023-07-18 15:06:11 +0100

    completed req 1, working on the tokens of req 2

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,11 +1,58 @@
+import { useState } from 'react';
 import './App.css';
 
 const apiUrl = 'http://localhost:4000';
+const InitialNewUser = {
+  username: '',
+  password: ''
+}
 
 function App() {
+  const [newUser, setNewUser] = useState(InitialNewUser)
+
+  const handleChange = (e) => {
+    const { name } = e.target
+    setNewUser({...newUser, [name]: e.target.value})
+    console.log(newUser)
+  }
+
+  const handleRegister = (e) => {
+    e.preventDefault()
+
+    const user = {
+      username: newUser.username,
+      password: newUser.password
+    }
+
+    const options = {
+      method: "post",
+      headers: {
+          "Content-Type": "application/json"
+      },
+      body: JSON.stringify(user)
+  }
+  
+  fetch(`${apiUrl}/user/register`, options)
+  .then(function (response) {
+      console.log('response returned...', response)
+      return response.json()
+  })
+  }
+
   return (
     <div className="App">
-
+      <h2>Register</h2>
+      <form onSubmit={handleRegister}>
+        <label>
+          Username: 
+          <input type="text" name='username' onChange={handleChange}/>
+        </label>
+        <label>
+          Password:
+          <input type="password" name='password' onChange={handleChange}/>
+        </label>
+          <input type="submit"/>
+      </form>
     </div>
   );
 }
---
+++ src/server/controllers/user.js
@@ -0,0 +1,45 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const prisma = require('../../utils/prisma.js')
+
+const secret = process.env.JWT_SECRET
+
+const registerUser = async (req, res) => {
+    const { password, username } = req.body
+    bcrypt.hash(password, 12, async function (err, hash) {
+        const newUser = await prisma.user.create({
+            data: {
+                username,
+                password: hash
+            },
+            select: {
+                username: true
+            }
+        })
+        console.log("this is newUser: ", newUser)
+        res.status(201).json({ user: newUser })
+    })
+}
+
+const loginUser = async (req, res) => {
+    const {username, password} = req.body
+    const foundUser = await prisma.user.findUnique({
+        where: {
+            username
+        }
+    })
+    if(foundUser){
+        bcrypt.compare(password, foundUser.password, function(err, result){
+            if(result){
+                const token = jwt.sign({username}, secret)
+                res.send({token})
+            } else res.status(401).send('Invalid username or password')
+        })
+    } else res.status(401).send('Invalid username or password')
+
+}
+
+module.exports = {
+    registerUser,
+    loginUser
+}
