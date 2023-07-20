e49de4ed5556e92d46bc21d6b2d50b99bf135ad5
Mantas Navickis <navickis@gmail.com>
2023-07-20 15:27:20 +0200

    error fixed

--- src/client/controllers/movie.js
+++ src/client/controllers/movie.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react'
 const apiUrl = 'http://localhost:4000'
 
 const Movie = ({ token }) => {
+
   const [movie, setMovie] = useState({
     title: '',
     description: '',
@@ -21,27 +22,23 @@ const Movie = ({ token }) => {
     }))
   }
 
-  useEffect(
-    () => {
-      const fetchMovies = async () => {
-        try {
-          const response = await fetch(`${apiUrl}/movie`, {
-            headers: {
-              Authorization: `Bearer ${token}`
-            }
-          })
-          const data = await response.json()
-          setMovies(data.movies)
-        } catch (error) {
-          console.error('Error:', error)
-        }
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const response = await fetch(`${apiUrl}/movie`, {
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        })
+        const data = await response.json()
+        setMovies(data.movies)
+      } catch (error) {
+        console.error('Error:', error)
       }
+    }
 
-      fetchMovies()
-    },
-    [token],
-    [movies]
-  )
+    fetchMovies()
+  }, [token])
 
   const createMovie = async (e) => {
     e.preventDefault()
@@ -64,7 +61,7 @@ const Movie = ({ token }) => {
             })
           } else if (response.status === 409) {
             return response.json().then(function (data) {
-              throw new Error('Movie with this title already exists')
+              throw new Error('Movie with this title already exists') 
             })
           } else {
             throw new Error('An unexpected error occurred')
@@ -74,6 +71,7 @@ const Movie = ({ token }) => {
         return response.json()
       })
       .then(function (data) {
+        setMovies((prevMovies) => [...prevMovies, data.movie])
         setMovieResponse('Movie has been created')
         setTimeout(() => {
           setMovieResponse('')
