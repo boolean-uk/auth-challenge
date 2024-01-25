759974ed51c2ba35aba073438dca42596106abf6
Chloe Zermatten <chloezermatten@gmail.com>
2024-01-25 16:17:21 +0000

    feat: handle register errors

--- src/server/controllers/user.js
+++ src/server/controllers/user.js
@@ -1,11 +1,20 @@
-import { registerDb } from "../domains/user.js"
-import { hashPassword } from "../utils/hash-password.js"
+import { registerDb } from "../domains/user.js";
+import { hashPassword } from "../utils/hash-password.js";
 
 const register = async (req, res) => {
-    const { username, password } = req.body
-    const hashedPassword = await hashPassword(password)
-    const registeredUser = await registerDb(username, hashedPassword)
-    res.status(201).json({message: `Welcome, ${registeredUser.username}!`})
-}
+  const { username, password } = req.body;
+  const hashedPassword = await hashPassword(password);
 
-export { register }
+  let registeredUser 
+  try {
+    registeredUser = await registerDb(username, hashedPassword);
+    res.status(201).json({ message: `Welcome, ${registeredUser.username}!` });
+    return
+  } catch(e) {
+    if (e.code === "P2002"){
+    res.status(409).json({error: "invalid username"})
+    }
+  }
+};
+
+export { register };
