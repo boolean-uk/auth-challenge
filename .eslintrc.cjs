274d484cc7e721a576af00ee1c8bb6afd7e97258
Eduard Bissell <edd.bissell@hotmail.co.uk>
2024-01-25 11:24:25 +0000

    build: add build settings

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,7 +4,12 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: ["eslint:recommended", "plugin:react/recommended", "prettier"],
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "prettier",
+  ],
   overrides: [
     {
       env: {
@@ -26,5 +31,10 @@ module.exports = {
     "linebreak-style": ["error", "unix"],
     quotes: ["error", "double"],
     semi: ["error", "always"],
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
   },
 };
