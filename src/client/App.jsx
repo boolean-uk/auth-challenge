694a0c67072254f21640f5e6910251a5eab288db
Eduard Bissell <edd.bissell@hotmail.co.uk>
2024-01-25 11:00:07 +0000

    style: format

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,13 +1,10 @@
-import './App.css';
+import "./App.css";
 
 const port = import.meta.env.VITE_PORT;
 const apiUrl = `http://localhost:${port}`;
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
