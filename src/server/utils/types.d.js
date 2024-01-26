298d854fd436c4d60af6d117336755455812d930
Eduard Bissell <edd.bissell@hotmail.co.uk>
2024-01-26 18:36:38 +0000

    feat: add typedefs

--- src/server/utils/types.d.js
+++ src/server/utils/types.d.js
@@ -1,8 +1,12 @@
 /**
  * @typedef {import("@prisma/client").User} User
  * @typedef {import("@prisma/client").Movie} Movie
+ * @typedef {import("@prisma/client").Prisma.PrismaClientKnownRequestError} PrismaError
  *
  * @typedef {import("express").Request} ExRequest
  * @typedef {import("express").Response} ExResponse
+ * @typedef {import("express").NextFunction} ExNext
+ *
+ * @typedef {{error: {formErrors: Object[], fieldErrors: {}, code: Number}}} ApiError
  */
 export const Types = {};
