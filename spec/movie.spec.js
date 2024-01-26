40f2112f8384b611d2ad02efda441fc6fe0071a9
Chloe Zermatten <chloezermatten@gmail.com>
2024-01-26 12:23:36 +0000

    test: fix create movie

--- spec/movie.spec.js
+++ spec/movie.spec.js
@@ -2,12 +2,12 @@
 import supertest from "supertest";
 import { app } from "../src/server/index.js";
 
-describe("POST/movies", () => {
-  const endpoint = "/movies";
+describe("POST/movie", () => {
+  const endpoint = "/movie";
   const token1 = "eyJhbGciOiJIUzI1NiJ9.R2VyYWx0.ma-8K_6d9R-WZ30yWxlgH0OeQLZ_wW90Q6EztwpeiOk";
-  const token2 = "";
-  const token3 = undefined;
-  const token4 = "this is not the token you are looking for";
+//   const token2 = "";
+//   const token3 = undefined;
+//   const token4 = "this is not the token you are looking for";
 
   const movie = {
     title: "The King's Speech",
@@ -16,7 +16,7 @@ describe("POST/movies", () => {
     runtimeMins: 119,
   };
   test("creates a movie", async () => {
-    const result = await supertest(app).post(endpoint).auth(token1).send(movie);
+    const result = await supertest(app).post(endpoint).set('Authorization', token1).send(movie);
     expect(result.status).toEqual(201);
     expect(result.body.movie.id).not.toBeUndefined();
     expect(result.body.movie.toMatchObject).toEqual();
