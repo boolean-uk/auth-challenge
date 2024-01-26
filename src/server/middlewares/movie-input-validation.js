b3c967eef6d65bec6c321984792e31c243fb6f86
Chloe Zermatten <chloezermatten@gmail.com>
2024-01-26 11:38:22 +0000

    feat: movie input validation

---
+++ src/server/middlewares/movie-input-validation.js
@@ -0,0 +1,14 @@
+const hasTitle = (title) => title && title.length;
+const hasDescription = (description) => description && description.length;
+const hasRuntimeMins = (runtimeMins) => runtimeMins && Number(runtimeMins);
+
+const validateInput = (movie) => {
+  const { title, description, runtimeMins } = movie;
+  return (
+    hasTitle(title) &&
+    hasDescription(description) &&
+    hasRuntimeMins(runtimeMins)
+  );
+};
+
+export { validateInput };
