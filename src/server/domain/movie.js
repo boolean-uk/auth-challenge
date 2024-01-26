5908c2a4a58add90134b9b061507bd991927e1bf
Lukas Dembicki <lad_777@hotmail.com>
2024-01-26 11:22:18 +0000

    BUILD: query to create movie

--- src/server/domain/movie.js
+++ src/server/domain/movie.js
@@ -1,3 +1,13 @@
 import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient();
 
+const createMovieDB = async (title, description, runtimeMins) => 
+await prisma.movie.create({
+    data: {
+        title, 
+        description, 
+        runtimeMins
+    }
+})
+
+export default createMovieDB
