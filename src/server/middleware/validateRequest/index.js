0c46a86f0cf9c6b580598557e2b399d80048f46e
Eduard Bissell <edd.bissell@hotmail.co.uk>
2024-01-26 18:38:08 +0000

    feat: add validateRequest middleware

---
+++ src/server/middleware/validateRequest/index.js
@@ -0,0 +1,20 @@
+import { handleError } from "../../utils/error.js";
+// eslint-disable-next-line no-unused-vars
+import * as Types from "../../utils/types.d.js";
+
+/**
+ * @param {import("zod").AnyZodObject} schema
+ * @returns {(req: Types.ExRequest, res: Types.ExResponse, next: Types.ExNext) => void}
+ */
+function validateRequest(schema) {
+  return function (req, res, next) {
+    try {
+      schema.parse(req.body);
+      next();
+    } catch (error) {
+      handleError(error, res);
+    }
+  };
+}
+
+export default validateRequest;
