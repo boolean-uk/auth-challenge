dd15721edbe323bd06d2d3d344d530e111117b80
Chloe Zermatten <chloezermatten@gmail.com>
2024-01-26 10:36:39 +0000

    test: login error handling

--- spec/user.spec.js
+++ spec/user.spec.js
@@ -1,9 +1,12 @@
+//TODO: use mock functions instead so that the tests do not rely on the db at all. 
+
 import supertest  from "supertest";
 import { app } from "../src/server";
 
 /* eslint-disable no-undef */
 describe("/user", () => { 
-  xdescribe("/register", () => {
+  describe("/register", () => {
+      const endpoint = "/user/register"
       const user1 = {
         username: "Geralt",
         password: "Roach",
@@ -22,7 +25,7 @@ describe("/user", () => {
       
     test("a new user is registered", async () => {
       const message = "Welcome, Geralt!"
-      const result = await supertest(app).post("/user/register").send(user1)
+      const result = await supertest(app).post(endpoint).send(user1)
 			expect(result.status).toEqual(201);
       expect(result.body.message).toEqual(message)
     });
@@ -33,49 +36,73 @@ describe("/user", () => {
       // somewhat redundant, seeing as the register page could serve to check that the username does exit
       // leaving only the password to figure out - or am I overthinking this entirely? 
       const message = "invalid username"
-      const result = await supertest(app).post("/user/register").send(user2)
+      const result = await supertest(app).post(endpoint).send(user2)
 			expect(result.status).toEqual(409);
       expect(result.body.error).toEqual(message)
     });
     test("a 400 error is thrown due to empty input fields", async () => {
       const message = "missing input"
-      const result = await supertest(app).post("/user/register").send(user3)
+      const result = await supertest(app).post(endpoint).send(user3)
 			expect(result.status).toEqual(400);
       expect(result.body.error).toEqual(message)
     });
     test("a 400 error is thrown due to missing input fields", async () => {
       const message = "missing input"
-      const result = await supertest(app).post("/user/register").send(user4)
+      const result = await supertest(app).post(endpoint).send(user4)
 			expect(result.status).toEqual(400);
       expect(result.body.error).toEqual(message)
     });
   });
   describe("/login", () => {
+    const endpoint = "/user/login"
     const user1 = {
       username: "Geralt",
       password: "Roach",
     };
+    const user2 = {
+      username: "Geralt",
+      password: "Roche",
+    };
+    const user3 = {
+      username: "Vernon",
+      password: "Roach",
+    };
+    const user4 = {
+      username: "",
+      password: "Roach",
+    };
+    const user5 = {
+      username: "Geralt",
+    };
     test("the user is logged in", async() => {
-      const result = await supertest(app).post("/user/login").send(user1)
+      const result = await supertest(app).post(endpoint).send(user1)
       expect(result.status).toEqual(201)
       expect(result.body.response.token).not.toBeUndefined()
       expect(typeof result.body.response.token).toEqual("string")
       expect(result.body.response.message).toEqual("login successful")
     })
-    // test("a 401 error is thrown as the password was incorrect")
-    // test("a 401 error is thrown as the username was incorrect")
-    // test("a 400 error is thrown due to empty input fields", async () => {
-    //   const message = "missing input"
-    //   const result = await supertest(app).post("/user/register").send(user3)
-		// 	expect(result.status).toEqual(400);
-    //   expect(result.body.error).toEqual(message)
-    // });
-    // test("a 400 error is thrown due to missing input fields", async () => {
-    //   const message = "missing input"
-    //   const result = await supertest(app).post("/user/register").send(user4)
-		// 	expect(result.status).toEqual(400);
-    //   expect(result.body.error).toEqual(message)
-    // });
+    test("a 401 error is thrown if the password is incorrect", async() => {
+      const result = await supertest(app).post(endpoint).send(user2)
+      expect(result.status).toEqual(401)
+      expect(result.body.error).toEqual("incorrect username/password")
+    })
+    test("a 401 error is thrown as the username is incorrect", async() => {
+      const result = await supertest(app).post(endpoint).send(user3)
+      expect(result.status).toEqual(401)
+      expect(result.body.error).toEqual("incorrect username/password")
+    })
+    test("a 400 error is thrown due to empty input fields", async () => {
+      const message = "missing input"
+      const result = await supertest(app).post(endpoint).send(user4)
+			expect(result.status).toEqual(400);
+      expect(result.body.error).toEqual(message)
+    });
+    test("a 400 error is thrown due to missing input fields", async () => {
+      const message = "missing input"
+      const result = await supertest(app).post(endpoint).send(user5)
+			expect(result.status).toEqual(400);
+      expect(result.body.error).toEqual(message)
+    });
 
   })
 });
