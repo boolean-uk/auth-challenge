6a4723608e0c57bb31063ecd5ba4ed3f087c63e1
Satoki <satokiito@gmail.com>
2024-01-26 14:14:22 +0000

    created MovieList component and extracted out

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
+import MovieList from './components/MovieList';
 import MovieForm from './components/MovieForm';
 import './App.css';
 
@@ -9,81 +10,8 @@ const port = import.meta.env.VITE_PORT;
 const apiUrl = `http://localhost:${port}`;
 
 function App() {
-  const [movies, setMovies] = useState([]);
-  const [createMovieMessage, setCreateMovieMessage] = useState('')
-  const [deleteAllMoviesMessage, setDeleteAllMoviesMessage] = useState('')
-
-  useEffect(() => {
-    fetch(`${apiUrl}/movie`)
-      .then(res => res.json())
-      .then(res => setMovies(res.data));
-  }, []);
-
-  const handleCreateMovie = async ({ title, description, runtimeMins }) => {
-    const token = localStorage.getItem('token')
-
-    try {
-      const { data } = await axios.post(`${apiUrl}/movie`, {
-        title,
-        description,
-        runtimeMins
-      }, {
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${token}`
-        }
-      }
-      )
-      const { movie, message } = data
-      setMovies([...movies, movie])
-      setCreateMovieMessage(message)
-    }
-    catch (err) {
-      setCreateMovieMessage(err.response.data.error)
-    }
-  }
-
-  const deleteAllMovies = async () => {
-    const token = localStorage.getItem('token')
-
-    try {
-      const { data } = await axios.delete(`${apiUrl}/movie`, {
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${token}`
-        }
-      })
-      setMovies([])
-      setDeleteAllMoviesMessage(data.message)
-    }
-    catch (err) {
-      setDeleteAllMoviesMessage(err.response.data.eror)
-    }
-  }
-
   return (
     <div className="App">
-
-      <h1>Create a movie</h1>
-      <MovieForm handleSubmit={handleCreateMovie} />
-      {createMovieMessage && <p>{createMovieMessage}</p>}
-
-      <h1>Movie list</h1>
-      <button className='delete-movies-btn' onClick={deleteAllMovies}>Delete all Movies</button>
-      {deleteAllMoviesMessage && <p>{deleteAllMoviesMessage}</p>}
-
-      <ul>
-        {movies.map(movie => {
-          return (
-            <li key={movie.id}>
-              <h3>{movie.title}</h3>
-              <p>Description: {movie.description}</p>
-              <p>Runtime: {movie.runtimeMins}</p>
-            </li>
-          );
-        })}
-      </ul>
-
       <Routes>
         <Route
           path='/'
@@ -92,7 +20,7 @@ function App() {
         </Route>
         <Route
           path='/movie-list'
-          element={<div>hi</div>}
+          element={<MovieList apiUrl={apiUrl} />}
         >
         </Route>
       </Routes>
---
+++ src/client/components/MovieList.jsx
@@ -0,0 +1,85 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+import MovieForm from "./MovieForm";
+
+function MovieList({ apiUrl }) {
+
+    const [movies, setMovies] = useState([]);
+    const [createMovieMessage, setCreateMovieMessage] = useState('')
+    const [deleteAllMoviesMessage, setDeleteAllMoviesMessage] = useState('')
+
+    useEffect(() => {
+        fetch(`${apiUrl}/movie`)
+          .then(res => res.json())
+          .then(res => setMovies(res.data));
+      }, []);
+
+    const handleCreateMovie = async ({ title, description, runtimeMins }) => {
+        const token = localStorage.getItem('token')
+    
+        try {
+          const { data } = await axios.post(`${apiUrl}/movie`, {
+            title,
+            description,
+            runtimeMins
+          }, {
+            headers: {
+              'Content-Type': 'application/json',
+              'Authorization': `Bearer ${token}`
+            }
+          }
+          )
+          const { movie, message } = data
+          setMovies([...movies, movie])
+          setCreateMovieMessage(message)
+        }
+        catch (err) {
+          setCreateMovieMessage(err.response.data.error)
+        }
+      }
+    
+      const deleteAllMovies = async () => {
+        const token = localStorage.getItem('token')
+    
+        try {
+          const { data } = await axios.delete(`${apiUrl}/movie`, {
+            headers: {
+              'Content-Type': 'application/json',
+              'Authorization': `Bearer ${token}`
+            }
+          })
+          setMovies([])
+          setDeleteAllMoviesMessage(data.message)
+        }
+        catch (err) {
+          setDeleteAllMoviesMessage(err.response.data.eror)
+        }
+      }
+
+    return (
+        <>
+            <h1>Create a movie</h1>
+            <MovieForm handleSubmit={handleCreateMovie} />
+            {createMovieMessage && <p>{createMovieMessage}</p>}
+    
+            <h1>Movie list</h1>
+            <button className='delete-movies-btn' onClick={deleteAllMovies}>Delete all Movies</button>
+            {deleteAllMoviesMessage && <p>{deleteAllMoviesMessage}</p>}
+    
+            <ul>
+            {movies.map(movie => {
+                return (
+                <li key={movie.id}>
+                    <h3>{movie.title}</h3>
+                    <p>Description: {movie.description}</p>
+                    <p>Runtime: {movie.runtimeMins}</p>
+                </li>
+                );
+            })}
+            </ul>
+        </>
+    )
+}
+
+export default MovieList
