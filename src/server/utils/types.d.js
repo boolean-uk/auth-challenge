7daf350ba888e67cc9df9a5ee2e8bdf45e218df5
Eduard Bissell <edd.bissell@hotmail.co.uk>
2024-01-26 13:25:43 +0000

    refactor: move typedefs

---
+++ src/server/utils/types.d.js
@@ -0,0 +1,8 @@
+/**
+ * @typedef {import("@prisma/client").User} User
+ * @typedef {import("@prisma/client").Movie} Movie
+ *
+ * @typedef {import("express").Request} ExRequest
+ * @typedef {import("express").Response} ExResponse
+ */
+export const Types = {};
