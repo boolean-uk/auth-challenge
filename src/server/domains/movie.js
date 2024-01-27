adc93b943664311674993d4ed34838fe746f5b39
Kye <NovusAa12@proton.me>
2024-01-27 14:52:25 +0000

    add domains/movie.js

---
+++ src/server/domains/movie.js
@@ -0,0 +1,15 @@
+import prisma from "../utils/prisma.js";
+
+const getAllMoviesDb = async () => await prisma.movie.findMany();
+
+const getMovieByTitleDb = async (title) =>
+  await prisma.movie.findUnique({
+    where: { title },
+  });
+
+const createMovieDb = async (title, description, runtimeMins) =>
+  await prisma.movie.create({
+    data: { title, description, runtimeMins },
+  });
+
+export { getAllMoviesDb, getMovieByTitleDb, createMovieDb };
