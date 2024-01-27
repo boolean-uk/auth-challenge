c283789d8a9aceb5629e31d88f845a0ff2312e10
Nazar Tymiv <nazartumiv@gmail.com>
2024-01-27 00:58:18 +0000

    Add main pages

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,13 +1,32 @@
-import './App.css';
+import { Navigate, Route, Routes } from 'react-router-dom'
+import './App.css'
+import axios from 'axios'
 
-const port = import.meta.env.VITE_PORT;
-const apiUrl = `http://localhost:${port}`;
+// Views
+import HomePage from './views/HomePage'
+import RegisterPage from './views/RegisterPage'
+import LoginPage from './views/LoginPage'
+import { useEffect, useState } from 'react'
+
+const port = import.meta.env.VITE_PORT
+const apiUrl = `http://localhost:${port}`
 
 function App() {
+  const [isAuth, setIsAuth] = useState(false)
+
   return (
-    <div className="App">
+    <div className="container">
+      <Routes>
+        <Route
+          path="/"
+          element={isAuth ? <HomePage /> : <Navigate to="/login" />}
+        />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
