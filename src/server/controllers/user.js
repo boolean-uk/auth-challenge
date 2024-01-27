75368b0b919e81c175d376a4a3f2c41163daeae8
Kye <NovusAa12@proton.me>
2024-01-27 13:21:24 +0000

    update controllers/user.js

--- src/server/controllers/user.js
+++ src/server/controllers/user.js
@@ -1,39 +1,49 @@
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient();
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { createUserDb, getUserByUsernameDb } from "../domains/user.js";
 
-const jwtSecret = 'mysecret';
+const jwtSecret = "mysecret";
 
 const register = async (req, res) => {
-    const { username, password } = req.body;
+  const { username, password } = req.body;
 
-    const createdUser = null;
+  if (!username || !password) {
+    return res.status(400).send({ error: "Missing fields in request body" });
+  }
 
-    res.json({ data: createdUser });
+  const usernameIsDuplicate = await getUserByUsernameDb(username);
+
+  if (usernameIsDuplicate) {
+    return res
+      .status(409)
+      .send({ error: "A user with the provided username already exists" });
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 12);
+
+  const createdUser = await createUserDb(username, hashedPassword);
+
+  return res.status(201).send({ data: createdUser });
 };
 
 const login = async (req, res) => {
-    const { username, password } = req.body;
+  const { username, password } = req.body;
 
-    const foundUser = null;
+  const foundUser = null;
 
-    if (!foundUser) {
-        return res.status(401).json({ error: 'Invalid username or password.' });
-    }
+  if (!foundUser) {
+    return res.status(401).json({ error: "Invalid username or password." });
+  }
 
-    const passwordsMatch = false;
+  const passwordsMatch = false;
 
-    if (!passwordsMatch) {
-        return res.status(401).json({ error: 'Invalid username or password.' });
-    }
+  if (!passwordsMatch) {
+    return res.status(401).json({ error: "Invalid username or password." });
+  }
 
-    const token = null;
+  const token = null;
 
-    res.json({ data: token });
+  res.json({ data: token });
 };
 
-export {
-    register,
-    login
-};
+export { register, login };
