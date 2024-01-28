f2b32811a14da1b640f2f388de75326f14aebb97
Elizabeth <lizbeepmc@gmail.com>
2024-01-28 11:27:34 +0300

    log in requirements

--- src/server/controllers/user.js
+++ src/server/controllers/user.js
@@ -9,12 +9,12 @@ const register = async (req, res) => {
   try {
     const { username, password } = req.body;
     const hashedPassword = await bcrypt.hash(password, 12);
-    const createdUser = await prisma.user.create ({
+    const createdUser = await prisma.user.create({
       data: {
-          username,
-          password:hashedPassword
-      }
-  });
+        username,
+        password: hashedPassword,
+      },
+    });
 
     res.status(201).json({ data: createdUser });
   } catch (error) {
@@ -26,21 +26,33 @@ const register = async (req, res) => {
 const login = async (req, res) => {
   const { username, password } = req.body;
 
-  const foundUser = null;
+  const foundUser = await prisma.user.findUnique({
+    where: {
+      username: username,
+    },
+  });
 
   if (!foundUser) {
     return res.status(401).json({ error: "Invalid username or password." });
   }
 
-  const passwordsMatch = false;
+  const passwordsMatch = await bcrypt.compare(password, foundUser.password);
 
   if (!passwordsMatch) {
     return res.status(401).json({ error: "Invalid username or password." });
+  } else {
+    const payload = { username: username };
+
+    const createToken = (payload, jwtSecret) => {
+      const token = jwt.sign(payload, jwtSecret);
+      return token;
+    };
+
+    const token = createToken(payload, jwtSecret);
+    res.json({ data: token });
+
+    console.log(token);
   }
-
-  const token = null;
-
-  res.json({ data: token });
 };
 
 export { register, login };
