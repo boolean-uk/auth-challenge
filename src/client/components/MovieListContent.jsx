2b118dfa6bf1bf24d7ec91477e5b5af01c4b3464
Satoki <satokiito@gmail.com>
2024-01-29 14:38:54 +0000

    extracted out separate component for create movie section

--- src/client/components/MovieListContent.jsx
+++ src/client/components/MovieListContent.jsx
@@ -1,37 +1,11 @@
 import axios from "axios";
 import { useState } from "react";
-import MovieForm from "./MovieForm"
+import CreateMovie from "./CreateMovie";
 import MovieListItems from "./MovieListItems"
 
 function MovieListContent({ apiUrl, movies, setMovies }) {
-
-    const [createMovieMessage, setCreateMovieMessage] = useState('')
     const [deleteAllMoviesMessage, setDeleteAllMoviesMessage] = useState('')
 
-    const handleCreateMovie = async ({ title, description, runtimeMins }) => {
-        const token = localStorage.getItem('token')
-    
-        try {
-          const { data } = await axios.post(`${apiUrl}/movie`, {
-            title,
-            description,
-            runtimeMins
-          }, {
-            headers: {
-              'Content-Type': 'application/json',
-              'Authorization': `Bearer ${token}`
-            }
-          }
-          )
-          const { movie, message } = data
-          setMovies([...movies, movie])
-          setCreateMovieMessage(message)
-        }
-        catch (err) {
-          setCreateMovieMessage(err.response.data.error)
-        }
-      }
-    
       const deleteAllMovies = async () => {
         const token = localStorage.getItem('token')
     
@@ -52,16 +26,12 @@ function MovieListContent({ apiUrl, movies, setMovies }) {
 
     return (
         <div className="movie-list--content-container grid">
-            <div className="movie-list--create-movie">
-            <h1>Create a movie</h1>
-            <MovieForm handleSubmit={handleCreateMovie} />
-            {createMovieMessage && <p>{createMovieMessage}</p>}
-            </div>
+            <CreateMovie apiUrl={apiUrl} movies={movies} setMovies={setMovies} />
             <div className="movie-list--movies-container grid">
-            <h1>Movie list</h1>
-            <button className='delete-movies-btn' onClick={deleteAllMovies}>Delete all Movies</button>
-            {deleteAllMoviesMessage && <p>{deleteAllMoviesMessage}</p>}
-            <MovieListItems movies={movies} />
+                <h1>Movie list</h1>
+                <button className='delete-movies-btn' onClick={deleteAllMovies}>Delete all Movies</button>
+                {deleteAllMoviesMessage && <p>{deleteAllMoviesMessage}</p>}
+                <MovieListItems movies={movies} />
             </div>
         </div>
     )
