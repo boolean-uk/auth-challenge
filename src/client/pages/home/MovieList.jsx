9ff6281429e459f673e4d839bdcb9c26c479d950
Chloe Zermatten <chloezermatten@gmail.com>
2024-01-29 17:48:45 +0000

    refactor: change folder structure

---
+++ src/client/pages/home/MovieList.jsx
@@ -0,0 +1,19 @@
+import { Movie } from "./Movie"
+
+const MovieList = ({ movies })  => {
+
+    return(
+        <>
+            <section>
+                <h2>Current list</h2>
+                <ul>
+                    {movies && movies.movies.toReversed().map((movie, index)=> (
+                        <Movie key={index + "movie"} movie={movie} />
+                    ))}
+                </ul>
+            </section>
+        </>
+    )
+}
+
+export { MovieList }
