f924778137c551e4d5974e5ae3e28d29949bd1c3
Pierluigi <pierluigi.capirci89@gmail.com>
2024-01-29 16:40:29 +0000

    update movies functionality

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -9,6 +9,8 @@ const apiUrl = `http://localhost:${port}`;
 function App() {
   const [movies, setMovies] = useState([]);
 
+  const [loginResponse, setLoginResponse] = useState(null);
+
   useEffect(() => {
     fetch(`${apiUrl}/movie`)
       .then((res) => res.json())
@@ -47,9 +49,36 @@ function App() {
     const userLoginData = await userData.json();
     const userToken = userLoginData.data;
     localStorage.setItem("token", userToken);
+    setLoginResponse(userToken);
   };
 
-  const handleCreateMovie = async ({ title, description, runtimeMins }) => {};
+  const handleCreateMovie = async ({ title, description, runtimeMins }) => {
+    try {
+      const token = localStorage.getItem("token");
+      if (!token) {
+        throw new Error("No token found!Please login first!");
+      }
+      const res = await fetch(`${apiUrl}/movie`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({ title, description, runtimeMins }),
+      });
+
+      const data = await res.json();
+
+      if (res.ok) {
+        setMovies((prevMovies) => [...prevMovies, data.data]);
+        console.log(data, token);
+      } else {
+        throw new Error(data.message || "Failed to create movie");
+      }
+    } catch (error) {
+      console.error("An error occurred during movie creation:", error.message);
+    }
+  };
 
   return (
     <div className="App">
@@ -58,13 +87,13 @@ function App() {
 
       <h1>Login</h1>
       <UserForm handleSubmit={handleLogin} />
-
+      {loginResponse && <h3>Login Success</h3>}
       <h1>Create a movie</h1>
       <MovieForm handleSubmit={handleCreateMovie} />
 
       <h1>Movie list</h1>
       <ul>
-        {movies.map((movie) => {
+        {loginResponse && movies.map((movie) => {
           return (
             <li key={movie.id}>
               <h3>{movie.title}</h3>
