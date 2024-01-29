16e5ca11a8fa901bb41e8a5b0b422d7f639736c5
Rebecca Noy <rebecca.noy@gmx.de>
2024-01-29 16:09:27 +0100

    trying to get server to start

--- src/server/controllers/user.js
+++ src/server/controllers/user.js
@@ -2,7 +2,7 @@ import { Prisma } from "@prisma/client"
 import { comparePassword } from "../helper/hashing"
 import { createUserDb } from "../domain/user"
 
-export const getUserByName = async (req, res) => {
+const getUserByName = async (req, res) => {
   const { username } = req.body
 
   try {
@@ -17,7 +17,7 @@ export const getUserByName = async (req, res) => {
   }
 }
 
-export const loginUser = async (req, res) => {
+const loginUser = async (req, res) => {
   try {
     const { username, password } = req.body
     const user = await getUserByName(req, res)
@@ -30,7 +30,7 @@ export const loginUser = async (req, res) => {
   }
 }
 
-export const createUser = async (req, res) => {
+const createUser = async (req, res) => {
   try {
     const { username } = req.body
     const usernameTaken = await getUserByName(req, res)
@@ -44,7 +44,7 @@ export const createUser = async (req, res) => {
   }
 }
 
-export const handleRegister = (username, password) => {
+const handleRegister = (username, password) => {
   try {
     const user = getUserByName(username)
     if (user) {
@@ -53,4 +53,11 @@ export const handleRegister = (username, password) => {
   } catch (error) {
     console.log(error)
   }
+}
+
+export { 
+  getUserByName,
+  loginUser,
+  createUser,
+  handleRegister
 }
--- src/server/routers/user.js
+++ src/server/routers/user.js
@@ -1,7 +1,8 @@
 import express from 'express';
-import { createUser } from '../controllers/user';
+import { createUser } from '../controllers/user.js';
 
 const router = express.Router();
+
 router.post('/register', createUser)
 
 export default router;
