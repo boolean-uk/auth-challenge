8db74ede436476f50250e8b7fa4c37155222b33c
Satoki <satokiito@gmail.com>
2024-01-29 14:56:30 +0000

    extracted out component to login user

--- src/client/components/Home.jsx
+++ src/client/components/Home.jsx
@@ -1,43 +1,14 @@
-import axios from "axios"
-import { useState } from "react"
-import { useNavigate } from "react-router-dom"
 import RegisterUser from "./RegisterUser"
-import UserForm from "./UserForm"
+import LoginUser from "./LoginUser"
 
 import '../styles/home.css'
 
 function Home({ apiUrl }) {
 
-    const navigate = useNavigate()
-
-    const [loginMessage, setLoginMessage] = useState('')
-      const handleLogin = async ({ username, password }) => {
-        try {
-          const { data } = await axios.post(`${apiUrl}/user/login`, {
-            username,
-            password
-          }, {
-              headers: { 'Content-Type': 'application/json' },
-            }
-          )
-          const { token, message } = data
-          localStorage.setItem('token', token)
-          setLoginMessage(message)
-          navigate('/movie-list')
-        }
-        catch (err) {
-          setLoginMessage(err.response.data.error)
-        }
-      };
-
     return (
         <section className="home--container grid">
           <RegisterUser apiUrl={apiUrl} />
-            <div className="home--form-container home--login-container grid">
-                <h1>Login</h1>
-                <UserForm handleSubmit={handleLogin} />
-                {loginMessage && <p>{loginMessage}</p>}
-            </div>
+          <LoginUser apiUrl={apiUrl} />
         </section>
     )
 }
---
+++ src/client/components/LoginUser.jsx
@@ -0,0 +1,37 @@
+import axios from "axios"
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+import UserForm from "./UserForm"
+
+function LoginUser({ apiUrl }) {
+    const navigate = useNavigate()
+    const [loginMessage, setLoginMessage] = useState('')
+    const handleLogin = async ({ username, password }) => {
+      try {
+        const { data } = await axios.post(`${apiUrl}/user/login`, {
+          username,
+          password
+        }, {
+            headers: { 'Content-Type': 'application/json' },
+          }
+        )
+        const { token, message } = data
+        localStorage.setItem('token', token)
+        setLoginMessage(message)
+        navigate('/movie-list')
+      }
+      catch (err) {
+        setLoginMessage(err.response.data.error)
+      }
+    };
+
+    return (
+        <div className="home--form-container home--login-container grid">
+            <h1>Login</h1>
+            <UserForm handleSubmit={handleLogin} />
+            {loginMessage && <p>{loginMessage}</p>}
+        </div>
+    )
+}
+
+export default LoginUser
