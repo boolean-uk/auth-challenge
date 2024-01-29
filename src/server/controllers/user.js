a5d9c9ae90614ec18ab0de201396694188d82bc7
tomlukrprogram <tomlukeprogramming@outlook.com>
2024-01-29 10:10:43 +0000

    req 2

--- src/server/controllers/user.js
+++ src/server/controllers/user.js
@@ -1,17 +1,37 @@
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient();
+import { PrismaClient } from '@prisma/client';
 
+const prisma = new PrismaClient();
 const jwtSecret = 'mysecret';
 
 const register = async (req, res) => {
-    const { username, password } = req.body;
+  const { username, password } = req.body;
 
-    const createdUser = null;
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-    res.json({ data: createdUser });
+    const createdUser = await prisma.user.create({
+      data: {
+        username,
+        password: hashedPassword,
+      },
+    });
+
+    const token = jwt.sign({ userId: createdUser.id }, jwtSecret);
+
+    res.json({
+      data: {
+        user: createdUser,
+        token,
+      },
+    });
+  } catch (error) {
+    console.error('Registration error:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 };
+
 
 const login = async (req, res) => {
     const { username, password } = req.body;
