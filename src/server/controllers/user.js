30615134d76a17f1ce047e6617c8eba2d7b8f191
Faiza <faraz_ul_islam@yahoo.com>
2024-01-29 23:05:55 +0000

    login- server

--- src/server/controllers/user.js
+++ src/server/controllers/user.js
@@ -23,5 +23,36 @@ const register = async (req, res) => {
   }
 };
 
+const login = async (req, res) => {
+  const { username, password } = req.body;
+
+  const foundUser = await prisma.user.findUnique({
+    where: {
+      username: username,
+    },
+  });
+
+  if (!foundUser) {
+    return res.status(401).json({ error: "Invalid username or password." });
+  }
+
+  const passwordsMatch = await bcrypt.compare(password, foundUser.password);
+
+  if (!passwordsMatch) {
+    return res.status(401).json({ error: "Invalid username or password." });
+  } else {
+    const payload = { username: username };
+
+    const createToken = (payload, jwtSecret) => {
+      const token = jwt.sign(payload, jwtSecret);
+      return token;
+    };
+
+    const token = createToken(payload, jwtSecret);
+    res.json({ data: token });
+
+    console.log(token);
+  }
+};
 
 export { register, login };
