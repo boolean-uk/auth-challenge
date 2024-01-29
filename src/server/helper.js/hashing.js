0fe0427aae088bcc62bffcc5a7488a615fac2e3e
Rebecca Noy <rebecca.noy@gmx.de>
2024-01-29 15:03:33 +0100

    add comparePassword

--- src/server/helper.js/hashing.js
+++ src/server/helper.js/hashing.js
@@ -4,3 +4,8 @@ export const authHashing = async (plainText) => {
   const hash = await bcrypt.hash(plainText, 12)
   return hash
 }
+
+export const comparePassword = async (plainText, hash) => {
+  const match = await bcrypt.compare(plainText, hash)
+  return match
+}
