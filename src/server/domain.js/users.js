15fe5f0f3bf5c39cf5f6bd6e678a41ba070f1487
Rebecca Noy <rebecca.noy@gmx.de>
2024-01-29 14:54:29 +0100

    add createUserDb

--- src/server/domain.js/users.js
+++ src/server/domain.js/users.js
@@ -1,5 +1,19 @@
 import { Prisma } from "@prisma/client"
 
+export const createUserDb = async (req, res) => {
+  const { username, password } = req.body
+
+  const user = await Prisma.user.create({
+    data: {
+      username,
+      password
+    }
+  })
+
+  res.status(201).json({ user })
+}
+
+
 export const getUserByNameDb = async (req, res) => {
   const { username } = req.body
 
