c896502bab625f793700cec191f2c37d38777962
Rebecca Noy <rebecca.noy@gmx.de>
2024-01-29 11:55:48 +0100

    general structure of Components for Landing page

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { handleRegister } from '../server/controllers/user';
 import './App.css';
 import SignUpForm from './components/SignUpForm'
+import LoginForm from './components/LoginForm';
 
 const port = import.meta.env.VITE_PORT;
 const apiUrl = `http://localhost:${port}`;
@@ -14,16 +15,30 @@ function App() {
   const [loginForm, setLoginForm] = useState(initForm)
   const [signUpForm, setSignUpForm] = useState(initForm)
 
-  const handleInput = (event) => {
+  const handleLoginInput = (event) => {
     const { name, value } = event.target
     setLoginForm({...loginForm, [name]: value})
+  }
+
+  const handleLoginSubmit = (event) => {
+    event.preventDefault()
+  }
+  
+  const handleRegisterInput = (event) => {
+    const { name, value } = event.target
     setSignUpForm({...loginForm, [name]: value})
+  }
+  
+  const handleRegisterSubmit = (event) => {
+    event.preventDefault()
   }
 
   return (
     <div className="App">
       <h1>Register an account</h1>
-      <SignUpForm handleInput={handleInput} handleSubmit={handleRegister} />
+      <SignUpForm handleInput={handleRegisterInput} handleSubmit={handleRegisterSubmit} />
+      <h1>Login to your account</h1>
+      <LoginForm handleInput={handleLoginInput} handleSubmit={handleLoginSubmit} />
     </div>
   );
 }
