91cfa69086c747a51d5238bd6c38ea6597276efe
Loza_TM <lozatmamo@gmail.com>
2024-01-30 23:11:15 +0100

    user registration and login with bcrypt and JWT

--- src/server/controllers/user.js
+++ src/server/controllers/user.js
@@ -1,39 +1,58 @@
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
-import { PrismaClient } from '@prisma/client'
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
-const jwtSecret = 'mysecret';
+const jwtSecret = "mysecret";
 
 const register = async (req, res) => {
+  try {
     const { username, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const createdUser = await prisma.user.create({
+      data: {
+        username,
+        password: hashedPassword,
+      },
+    });
 
-    const createdUser = null;
-
-    res.json({ data: createdUser });
+    res.status(201).json({ data: createdUser });
+  } catch (error) {
+    console.error("Error during registration:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
 };
 
 const login = async (req, res) => {
-    const { username, password } = req.body;
+  const { username, password } = req.body;
 
-    const foundUser = null;
+  const foundUser = await prisma.user.findUnique({
+    where: {
+      username: username,
+    },
+  });
 
-    if (!foundUser) {
-        return res.status(401).json({ error: 'Invalid username or password.' });
-    }
+  if (!foundUser) {
+    return res.status(401).json({ error: "Invalid username or password." });
+  }
 
-    const passwordsMatch = false;
+  const passwordsMatch = await bcrypt.compare(password, foundUser.password);
 
-    if (!passwordsMatch) {
-        return res.status(401).json({ error: 'Invalid username or password.' });
-    }
+  if (!passwordsMatch) {
+    return res.status(401).json({ error: "Invalid username or password." });
+  } else {
+    const payload = { username: username };
 
-    const token = null;
+    const createToken = (payload, jwtSecret) => {
+      const token = jwt.sign(payload, jwtSecret);
+      return token;
+    };
 
+    const token = createToken(payload, jwtSecret);
     res.json({ data: token });
+
+    console.log(token);
+  }
 };
 
-export {
-    register,
-    login
-};
+export { register, login };
