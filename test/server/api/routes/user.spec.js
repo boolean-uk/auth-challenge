3790319014ca87978dc10a40a32e848e6749c19f
Eduard Bissell <edd.bissell@hotmail.co.uk>
2024-01-30 10:20:38 +0000

    test: add /user/login/ tests

--- test/server/api/routes/user.spec.js
+++ test/server/api/routes/user.spec.js
@@ -1,6 +1,7 @@
 import supertest from "supertest";
 import app from "../../../../src/server/server.js";
 import { createUser } from "../../../../src/server/domains/user.domain";
+import { hashString } from "../../../../src/server/utils/hash.js";
 
 describe("User Endpoint", () => {
   describe("POST /user/register", () => {
@@ -82,6 +83,155 @@ describe("User Endpoint", () => {
         .send(addedField);
       const responseIncorrectTypes = await supertest(app)
         .post("/user/register")
+        .send(incorrectTypes);
+
+      const expectedMissingAll = {
+        error: {
+          code: 400,
+          formErrors: [],
+          fieldErrors: {
+            username: ["Required"],
+            password: ["Required"],
+          },
+        },
+      };
+      const expectedMissingPassword = {
+        error: {
+          code: 400,
+          formErrors: [],
+          fieldErrors: {
+            password: ["Required"],
+          },
+        },
+      };
+      const expectedMissingUsername = {
+        error: {
+          code: 400,
+          formErrors: [],
+          fieldErrors: {
+            username: ["Required"],
+          },
+        },
+      };
+      const expectedAddedField = {
+        error: {
+          code: 400,
+          formErrors: ["Unrecognized key(s) in object: 'title'"],
+          fieldErrors: {},
+        },
+      };
+      const expectedIncorrectTypes = {
+        error: {
+          formErrors: [],
+          fieldErrors: {
+            username: ["Expected string, received number"],
+            password: ["Expected string, received number"],
+          },
+          code: 400,
+        },
+      };
+
+      expect(responseMissingAll.status).toEqual(400);
+      expect(responseMissingPassword.status).toEqual(400);
+      expect(responseMissingUsername.status).toEqual(400);
+      expect(responseAddedField.status).toEqual(400);
+      expect(responseIncorrectTypes.status).toEqual(400);
+      expect(responseMissingAll.body).toEqual(expectedMissingAll);
+      expect(responseMissingPassword.body).toEqual(expectedMissingPassword);
+      expect(responseMissingUsername.body).toEqual(expectedMissingUsername);
+      expect(responseAddedField.body).toEqual(expectedAddedField);
+      expect(responseIncorrectTypes.body).toEqual(expectedIncorrectTypes);
+    });
+  });
+
+  describe("POST /user/login", () => {
+    it("will return jwt", async () => {
+      await createUser("bob", await hashString("builder"));
+      const request = {
+        username: "bob",
+        password: "builder",
+      };
+
+      const response = await supertest(app).post("/user/login").send(request);
+
+      const expectedToken = {
+        header: { alg: "HS256", typ: "JWT" },
+        payload: {
+          sub: "login",
+          username: "bob",
+          iat: expect.any(Number),
+          exp: expect.any(Number),
+        },
+      };
+
+      const [receivedTokenHeader, receivedTokenPayload] = response.body.token
+        .split(".")
+        .map((part) => {
+          try {
+            return JSON.parse(atob(part));
+          } catch {
+            return part;
+          }
+        });
+
+      expect(response.status).toEqual(201);
+      expect(receivedTokenHeader).toEqual(expectedToken.header);
+      expect(receivedTokenPayload).toEqual(expectedToken.payload);
+    });
+
+    it("will return 401 when incorrect login details are provided", async () => {
+      await createUser("bob", await hashString("builder"));
+      const request = {
+        username: "bob",
+        password: "plumber",
+      };
+
+      const response = await supertest(app).post("/user/login").send(request);
+
+      const expected = {
+        error: {
+          formErrors: ["Unrecognized username and password combination"],
+          fieldErrors: {},
+          code: 401,
+        },
+      };
+
+      expect(response.status).toEqual(401);
+      expect(response.body).toEqual(expected);
+    });
+
+    it("will return 400 when request is malformed", async () => {
+      const requestMissingAll = {};
+      const requestMissingPassword = {
+        username: "bob",
+      };
+      const requestMissingUsername = {
+        password: "builder",
+      };
+      const addedField = {
+        username: "bob",
+        password: "builder",
+        title: "builder",
+      };
+      const incorrectTypes = {
+        username: 123,
+        password: 456,
+      };
+
+      const responseMissingAll = await supertest(app)
+        .post("/user/login")
+        .send(requestMissingAll);
+      const responseMissingPassword = await supertest(app)
+        .post("/user/login")
+        .send(requestMissingPassword);
+      const responseMissingUsername = await supertest(app)
+        .post("/user/login")
+        .send(requestMissingUsername);
+      const responseAddedField = await supertest(app)
+        .post("/user/login")
+        .send(addedField);
+      const responseIncorrectTypes = await supertest(app)
+        .post("/user/login")
         .send(incorrectTypes);
 
       const expectedMissingAll = {
