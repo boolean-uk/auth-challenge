ed54c59918201f5befe29dea51fc75cd2b81f902
Rebecca Noy <rebecca.noy@gmx.de>
2024-01-31 12:46:47 +0100

    some changes

--- src/server/routers/user.js
+++ src/server/routers/user.js
@@ -3,6 +3,7 @@ const router = express.Router();
 import { createUser, loginUser } from '../controllers/user.js';
 
 router.post('/register', createUser)
+
 router.post('/login', loginUser)
 
 export default router;
