958ce3543360dbe9d24b239bfd713a95991525e7
Kye <NovusAa12@proton.me>
2024-02-01 15:42:21 +0000

    update LoginPage component

--- src/client/pages/Login/index.jsx
+++ src/client/pages/Login/index.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import UserForm from "../../components/UserForm";
 import api from "../../../server/api/axios.js";
-import "./index.css";
+import "../forms.css";
 
 export default function LoginPage() {
   const [loginMessage, setLoginMessage] = useState("");
+
+  const navigate = useNavigate();
 
   const handleLogin = async ({ username, password }) => {
     try {
@@ -20,13 +22,19 @@ export default function LoginPage() {
   };
 
   return (
-    <div className="login-page">
-      <h1>Login</h1>
-      <p>
-        No account? <Link to="/register">Register</Link> now!
-      </p>
-      <UserForm handleSubmit={handleLogin} />
-      {loginMessage && <p>{loginMessage}</p>}
+    <div className="login-page form-page">
+      <button className="back-btn" onClick={() => navigate(-1)}>
+        Go back
+      </button>
+
+      <div className="login-page--form form-container">
+        <h1>Login</h1>
+        <p>
+          No account? <Link to="/register">Register</Link> now!
+        </p>
+        <UserForm handleSubmit={handleLogin} />
+        {loginMessage && <p className="error-message">{loginMessage}</p>}
+      </div>
     </div>
   );
 }
