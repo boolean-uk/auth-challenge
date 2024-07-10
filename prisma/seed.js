c83ca7962d36a9e27bc6ea336ce622cc33afa736
Will Baxter <bax.will@googlemail.com>
2024-07-10 13:54:47 +0100

    data seed file

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,5 +1,40 @@
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
-import bcrypt from 'bcrypt'
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+import bcrypt from "bcrypt";
 
-console.log(prisma)
+async function seedUsers() {
+  await prisma.user.create({
+    data: {
+      username: "Jonny",
+      passwordHash: await bcrypt.hash("password123", 8),
+      movies: {
+        create: {
+          title: "Top Gun",
+          description: "Fighter jets and stuff",
+          runtimeMins: 89,
+        },
+      },
+    },
+  });
+
+  await prisma.user.create({
+    data: {
+      username: "Mikey",
+      passwordHash: await bcrypt.hash("password321", 8),
+      movies: {
+        createMany: {
+          data: [
+            { title: "Marley And Me", description: "Labradors And Stuff", runtimeMins: 89 },
+            {
+              title: "28 Days Later",
+              description: "Zombies and stuff",
+              runtimeMins: 89,
+            },
+          ],
+        },
+      },
+    },
+  });
+}
+
+seedUsers();
