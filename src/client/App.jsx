81d5a2b9bdf8155d8006248c9a2c866bb0a66ecd
Myrthe Dullaart <myrtheluca@hotmail.com>
2024-07-10 11:21:58 +0200

    set up project

---
+++ src/client/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+const port = import.meta.env.VITE_PORT
+const apiUrl = `http://localhost:${port}`
+
+function App() {
+  return (
+    <>
+    </>
+  )
+}
+
+export default App
